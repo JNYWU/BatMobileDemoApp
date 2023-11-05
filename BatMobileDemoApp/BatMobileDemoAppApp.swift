5314da094b632377a8ccf4e242d30f02b4512c33
jnywu <johnnywu203@gmail.com>
2023-11-05 19:35:36 +0800

    Initial Commit

---
+++ BatMobileDemoApp/BatMobileDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatMobileDemoAppApp.swift
+//  BatMobileDemoApp
+//
+//  Created by 吳求元 on 2023/11/5.
+//
+
+import SwiftUI
+
+@main
+struct BatMobileDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
